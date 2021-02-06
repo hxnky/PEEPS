536504737e0c0b39a2d65062a6894edf3e9e32b9
Khyo0 <71997990+Khyo0@users.noreply.github.com>
2021-02-07 01:17:55 +0900

    DB 연결 중

--- spring/Peeps_Chat/src/main/java/com/gnjk/chat/controller/ChattingController.java
+++ spring/Peeps_Chat/src/main/java/com/gnjk/chat/controller/ChattingController.java
@@ -1,12 +1,16 @@
 package com.gnjk.chat.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.gnjk.chat.service.MessageService;
 
 @Controller
 public class ChattingController { // 채팅 화면 (*수정 중)  
@@ -22,6 +26,17 @@ public class ChattingController { // 채팅 화면 (*수정 중)
 		session.setAttribute("user", userid);
 
 		return mav;
+	}
+	
+	@Autowired
+	private MessageService messageService;
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(HttpServletRequest request, Model model) {
+		
+		// 서비스 메소드 호출
+		messageService.save(request);
+		
 	}
 	
 }
--- spring/Peeps_Chat/src/main/java/com/gnjk/chat/dao/IMessageDao.java
+++ spring/Peeps_Chat/src/main/java/com/gnjk/chat/dao/IMessageDao.java
@@ -2,32 +2,10 @@ package com.gnjk.chat.dao;
 
 import com.gnjk.chat.domain.Message;
 
+//Chat 테이블 작업을 위한 메소드를 선언하는 인터페이스
 public interface IMessageDao {
 
-	/*
-	// public abstract
-		// 회원 등록
-		int insertMember(Member member);
-
-		// 전체 회원의 수
-		int memberCountUpdate();
-
-		// 로그인
-		Member selectLogin(String id, String pw);
-
-		// 회원의 총 수
-		int selectTotalCount();
-
-		// 회원 리스트(페이지 번호에 맞는)
-		//List<Member> selectMemberList(int startRow, int cntPerPage);
-		List<Member> selectMemberList(Map<String , Object> param);
-
-		// 2021.01.28 : Rest API : GET
-		List<Member> selectAllMemberList();
-	*/
+	// 메세지 저장을 위한 메소드
+	public void send(Message vo);
 	
-	// 메세지 등록
-	int insertMessage(Message message);
-	
-	// 메세지 리스트
 }
