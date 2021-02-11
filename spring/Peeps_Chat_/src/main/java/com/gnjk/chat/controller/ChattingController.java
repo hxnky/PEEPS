132c807ae2509176ce4319446519a5057babaadf
NamSeoA <71997900+NamSeoA@users.noreply.github.com>
2021-02-12 01:20:27 +0900

    update

---
+++ spring/Peeps_Chat_/src/main/java/com/gnjk/chat/controller/ChattingController.java
@@ -0,0 +1,39 @@
+package com.gnjk.chat.controller;
+
+
+import java.util.Date;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.socket.WebSocketSession;
+
+@Controller
+public class ChattingController { 
+
+	@RequestMapping("/views/chatting")
+	public ModelAndView chat(
+			ModelAndView mav, 
+			@RequestParam("m_idx") String m_idx,
+			Date ch_time,
+			//HttpSession session,
+			WebSocketSession socksession
+			) {
+
+		mav.setViewName("chatting");
+
+		//mav.addObject("m_idx", m_idx);
+		mav.addObject("rm_idx", "rm_idx");
+
+		//session.setAttribute("m_idx", m_idx);
+		
+		String chatMember = (String) socksession.getAttributes().get("m_idx" );
+		mav.addObject("m_idx", chatMember);
+		
+		return mav;
+	}
+
+}
