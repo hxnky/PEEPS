9da8ce51f5dd64876dcf54c7545247f38f532276
Jung Hyun Kim <jnghyn93@gmail.com>
2021-02-17 18:15:30 +0900

    컨트롤러 정리

--- spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/controller/MainViewController.java
+++ spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/controller/MainViewController.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainViewController {
 	
+	// 게시글 작성 폼
+	@RequestMapping("/main/post/upload")
+	public String goWriteForm() {
+		return "post/writeForm";
+	}
+	
 	// 마이페이지의 게시글 리스트
 	@RequestMapping("/main/jhS2")	// test 회원 아이디를 세션에서 받아야 함
 	public String goMemberMypage(
 			@RequestParam(value = "p", defaultValue = "1") int p
 			) {
 		return "post/myPage";
-	}
-	
-	// 게시글 작성 폼
-	@RequestMapping("/main/post/upload")
-	public String goWriteForm() {
-		return "post/writeForm";
 	}
 	
 	// 게시글 상세보기
--- spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/controller/PostController.java
+++ spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.gnjk.post.mypost.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,80 +18,89 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.gnjk.post.mypost.domain.Post;
+import com.gnjk.post.mypost.domain.PostFile;
 import com.gnjk.post.mypost.domain.PostListView;
 import com.gnjk.post.mypost.domain.PostWriteRequest;
 import com.gnjk.post.mypost.service.PostDeleteService;
 import com.gnjk.post.mypost.service.PostEditService;
 import com.gnjk.post.mypost.service.PostListService;
 import com.gnjk.post.mypost.service.PostReadService;
-import com.gnjk.post.mypost.service.PostWriteService;
+import com.gnjk.post.mypost.service.PostUploadService;
 
 @RestController
 @RequestMapping("rest/member/post")
 public class PostController {
-	
+
 	@Autowired
 	private PostListService listService;
 	@Autowired
-	private PostWriteService writeService;
+	private PostUploadService uploadService;
 	@Autowired
 	private PostReadService readService;
 	@Autowired
 	private PostDeleteService deleteService;
 	@Autowired
 	private PostEditService editService;
-	
-	// 게시글 리스트 출력
-	@GetMapping("/list")
-	public PostListView postList(
-			@RequestParam(value = "p", defaultValue = "1") int page,
-			Model model
-			) {
-		
-//		model.addAttribute("listView", listService.getPostListView(page));
-		
-		return listService.getPostListView(page);
-	}
-	
-	// 게시글 하나 출력
-	@GetMapping("/detail")
-	public Post getPostDetail() {
-		return new Post();
-	}
-	
-	// 게시글 작성 페이지
+
+	// 게시글 업로드
 	@PostMapping("/upload")
 	@ResponseBody
-	public int uploadPost() {
-		
-		return 0;
+	public int uploadPost(PostWriteRequest writeRequest, HttpServletRequest request, Model model) {
+
+		return uploadService.upload(writeRequest, request, model);
 	}
-	
-	// 게시글 내용 등록 처리
-	@RequestMapping(value = "/write", method = RequestMethod.POST)
-	public String postWritePOST(
-			@ModelAttribute("writeData") PostWriteRequest writeRequest,
-			HttpServletRequest request,
-			Model model
+
+	// 게시글 리스트 출력
+	@GetMapping("/list")
+	public PostListView postList(@RequestParam(value = "p", defaultValue = "1") int page, Model model) {
+
+//		model.addAttribute("listView", listService.getPostListView(page));
+
+		return listService.getPostListView(page);
+	}
+
+	// 게시글 하나 출력
+	@GetMapping("/detail")
+	public Post getPostDetail(
+			@RequestParam("idx") int postIdx
 			) {
-		
-		System.out.println(writeRequest);
-		
-		int result = writeService.postWrite(writeRequest, request);
-		
-		model.addAttribute("result", result);
-		
-		/* return "/mypost/postWriteView"; */
-		return "redirect:/post/list";
+		return listService.getDetail(postIdx);
 	}
 	
+	// 게시글의 이미지 출력
+	@GetMapping("/detail/image")
+	public List<PostFile> getPostDetailImgs(
+			@RequestParam("idx") int postIdx
+			){
+		
+		return listService.getDetailImgs(postIdx);
+	}
+
+	// 게시글 내용 등록 처리
+//	@RequestMapping(value = "/write", method = RequestMethod.POST)
+//	public String postWritePOST(
+//			@ModelAttribute("writeData") PostWriteRequest writeRequest,
+//			HttpServletRequest request,
+//			Model model
+//			) {
+//		
+//		System.out.println(writeRequest);
+//		
+//		int result = uploadService.postWrite(writeRequest, request);
+//		
+//		model.addAttribute("result", result);
+//		
+//		/* return "/mypost/postWriteView"; */
+//		return "redirect:/post/list";
+//	}
+
 	// TEST게시글 보기 페이지
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
 	public String postRead() {
-		
+
 		return "/mypost/postRead";
 	}
-	
+
 	// 게시글 조회
 //	@RequestMapping(value = "/postNO={p_idx}", method = RequestMethod.GET)
 //	public String postReadGET(
@@ -102,36 +113,26 @@ public class PostController {
 //		
 //		return "/mypost/postRead";
 //	}
-	
+
 	// 게시글 수정페이지
 	@RequestMapping(value = "/editPNO={p_idx}", method = RequestMethod.GET)
-	public String postEditForm(
-			@PathVariable("p_idx") int pidx,
-			Model model
-			) {
-		
+	public String postEditForm(@PathVariable("p_idx") int pidx, Model model) {
+
 		model.addAttribute("editView", editService.getPost(pidx));
-		
+
 		return "/mypost/postEditForm";
 	}
-	
+
 	// 게시글 수정처리
-	
-	
+
 	// 게시글 삭제처리
 	@RequestMapping(value = "/deletePNO={pidx}", method = RequestMethod.GET)
-	public String deletePost(
-			@PathVariable("pidx") int pidx,
-			Model model
-			) {
+	public String deletePost(@PathVariable("pidx") int pidx, Model model) {
 //		System.out.println("글 번호 : "+pidx);
-		
+
 		model.addAttribute("result", deleteService.deletePost(pidx));
-		
+
 		return "redirect:/post/list";
 	}
-	
-		
-		
 
 }
