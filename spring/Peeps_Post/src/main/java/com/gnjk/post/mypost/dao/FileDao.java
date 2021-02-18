3039c02eca4e1dada10614205af716721cd35975
Jung Hyun Kim <jnghyn93@gmail.com>
2021-02-19 01:23:02 +0900

    FileDao 이미지 삭제 추가

--- spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/dao/FileDao.java
+++ spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/dao/FileDao.java
@@ -10,5 +10,7 @@ public interface FileDao {
 	int insertFiles(PostFile postfile);
 	// 게시글 이미지 불러오기
 	List<PostFile> selectPostImgs(int idx);
+	// 수정하면서 삭제한 게시글의 기존 이미지 삭제
+	int deleteBeforeImage(String fname);
 
 }
