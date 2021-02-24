b6a0ba968d26e3dbad19e0f0a37b8f437a8977a1
Jung Hyun Kim <jnghyn93@gmail.com>
2021-02-25 01:06:10 +0900

    게시글삭제 service 수정

--- spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/service/PostDeleteService.java
+++ spring/Peeps_Post/src/main/java/com/gnjk/post/mypost/service/PostDeleteService.java
@@ -1,5 +1,7 @@
 package com.gnjk.post.mypost.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
