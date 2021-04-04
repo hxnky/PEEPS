a7e9958a031ae5a7a33f787286592e791743629c
NamSeoA <71997900+NamSeoA@users.noreply.github.com>
2021-04-04 20:04:24 +0900

    for recommit

--- spring/Peeps/src/main/java/com/gnjk/peeps/Member/dao/AlarmDao.java
+++
@@ -1,9 +0,0 @@
-package com.gnjk.peeps.Member.dao;
-
-import com.gnjk.peeps.Member.domain.Alarm;
-
-public interface AlarmDao {
-	
-	public void insertAlarm(Alarm alarm) throws Exception;
-
-}
--- spring/Peeps/src/main/java/com/gnjk/peeps/Member/dao/MessageDao.java
+++
@@ -1,9 +0,0 @@
-package com.gnjk.peeps.Member.dao;
-
-import com.gnjk.peeps.Member.domain.Message;
-
-public interface MessageDao {
-	
-	public void insertMessage(Message mes) throws Exception;
-	
-}
